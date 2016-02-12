b06805cfb0d8569d32c87a750f1a4419e15b4dd9
Mark Zolton <mark@zolton.org>
2016-02-11 21:34:21 -0600

    Initial commit.

---
+++ SavedVariables/ZO_Ingame.lua
@@ -0,0 +1,546 @@
+ZO_Ingame_SavedVariables =
+{
+    ["Default"] = 
+    {
+        ["@blakdeth913"] = 
+        {
+            ["$AccountWide"] = 
+            {
+                ["GamepadInventory"] = 
+                {
+                    ["version"] = 2,
+                    ["useStatComparisonTooltip"] = true,
+                },
+                ["GuildMotD"] = 
+                {
+                    ["version"] = 1,
+                },
+            },
+            ["Cat'Damon"] = 
+            {
+                ["PerformanceMeters"] = 
+                {
+                    ["version"] = 1,
+                    ["x"] = -20,
+                    ["point"] = 6,
+                    ["y"] = 20,
+                    ["relPoint"] = 6,
+                },
+                ["DeathRecap"] = 
+                {
+                    ["version"] = 1,
+                    ["recapOn"] = true,
+                },
+                ["SmithingCreation"] = 
+                {
+                    ["version"] = 2,
+                    ["haveMaterialChecked"] = false,
+                    ["haveKnowledgeChecked"] = true,
+                },
+                ["Dyeing"] = 
+                {
+                    ["sortStyle"] = 1,
+                    ["version"] = 1,
+                    ["showLocked"] = true,
+                },
+                ["Provisioner"] = 
+                {
+                    ["version"] = 1,
+                    ["haveSkillsChecked"] = true,
+                    ["haveIngredientsChecked"] = true,
+                },
+                ["DurabilityWarner"] = 
+                {
+                    ["version"] = 1,
+                },
+                ["AutoComplete"] = 
+                {
+                    ["version"] = 3,
+                    ["RecentInteractions"] = 
+                    {
+                    },
+                },
+                ["GamepadProvisioner"] = 
+                {
+                    ["version"] = 2,
+                    ["haveSkillsChecked"] = false,
+                    ["haveIngredientsChecked"] = false,
+                },
+                ["WorldMap"] = 
+                {
+                    ["version"] = 4,
+                    [1] = 
+                    {
+                        ["width"] = 488,
+                        ["x"] = 0,
+                        ["mapSize"] = 2,
+                        ["point"] = 128,
+                        ["relPoint"] = 128,
+                        ["filters"] = 
+                        {
+                            [1] = 
+                            {
+                            },
+                            [2] = 
+                            {
+                                [1] = false,
+                                [6] = false,
+                                [11] = 1,
+                            },
+                            [3] = 
+                            {
+                            },
+                        },
+                        ["y"] = 0,
+                        ["keepSquare"] = true,
+                        ["height"] = 550,
+                    },
+                    [2] = 
+                    {
+                        ["mapSize"] = 1,
+                        ["filters"] = 
+                        {
+                            [1] = 
+                            {
+                            },
+                            [2] = 
+                            {
+                                [1] = false,
+                                [6] = false,
+                                [11] = 1,
+                            },
+                            [3] = 
+                            {
+                            },
+                        },
+                    },
+                    [3] = 
+                    {
+                        ["mapSize"] = 1,
+                        ["filters"] = 
+                        {
+                            [2] = 
+                            {
+                                [8] = false,
+                                [1] = false,
+                                [3] = false,
+                                [4] = false,
+                                [5] = false,
+                                [11] = 1,
+                                [7] = false,
+                            },
+                        },
+                        ["allowHistory"] = false,
+                    },
+                    [4] = 
+                    {
+                        ["mapSize"] = 1,
+                        ["filters"] = 
+                        {
+                            [1] = 
+                            {
+                            },
+                            [2] = 
+                            {
+                                [3] = false,
+                                [2] = false,
+                                [11] = 1,
+                            },
+                            [3] = 
+                            {
+                            },
+                        },
+                        ["allowHistory"] = false,
+                    },
+                    [5] = 
+                    {
+                        ["mapSize"] = 1,
+                        ["filters"] = 
+                        {
+                            [2] = 
+                            {
+                                [8] = false,
+                                [1] = false,
+                                [3] = false,
+                                [11] = 1,
+                            },
+                            [3] = 
+                            {
+                            },
+                        },
+                        ["allowHistory"] = false,
+                    },
+                    ["userMode"] = 2,
+                },
+                ["Chat"] = 
+                {
+                    ["containers"] = 
+                    {
+                        [1] = 
+                        {
+                            ["relPoint"] = 6,
+                            ["width"] = 445,
+                            ["height"] = 267,
+                            ["point"] = 6,
+                            ["y"] = -82,
+                            ["x"] = 0,
+                        },
+                    },
+                    ["version"] = 4,
+                },
+            },
+            ["Randomcatperson"] = 
+            {
+                ["PerformanceMeters"] = 
+                {
+                    ["version"] = 1,
+                    ["x"] = -20,
+                    ["point"] = 6,
+                    ["y"] = 20,
+                    ["relPoint"] = 6,
+                },
+                ["DeathRecap"] = 
+                {
+                    ["version"] = 1,
+                    ["recapOn"] = true,
+                },
+                ["SmithingCreation"] = 
+                {
+                    ["version"] = 2,
+                    ["haveMaterialChecked"] = false,
+                    ["haveKnowledgeChecked"] = true,
+                },
+                ["Dyeing"] = 
+                {
+                    ["sortStyle"] = 1,
+                    ["version"] = 1,
+                    ["showLocked"] = true,
+                },
+                ["Provisioner"] = 
+                {
+                    ["version"] = 1,
+                    ["haveSkillsChecked"] = true,
+                    ["haveIngredientsChecked"] = true,
+                },
+                ["DurabilityWarner"] = 
+                {
+                    ["version"] = 1,
+                },
+                ["AutoComplete"] = 
+                {
+                    ["version"] = 3,
+                    ["RecentInteractions"] = 
+                    {
+                    },
+                },
+                ["GamepadProvisioner"] = 
+                {
+                    ["version"] = 2,
+                    ["haveSkillsChecked"] = false,
+                    ["haveIngredientsChecked"] = false,
+                },
+                ["WorldMap"] = 
+                {
+                    ["version"] = 4,
+                    [1] = 
+                    {
+                        ["width"] = 488,
+                        ["x"] = 0,
+                        ["mapSize"] = 2,
+                        ["point"] = 128,
+                        ["relPoint"] = 128,
+                        ["filters"] = 
+                        {
+                            [1] = 
+                            {
+                            },
+                            [2] = 
+                            {
+                                [1] = false,
+                                [6] = false,
+                                [11] = 1,
+                            },
+                            [3] = 
+                            {
+                            },
+                        },
+                        ["y"] = 0,
+                        ["keepSquare"] = true,
+                        ["height"] = 550,
+                    },
+                    [2] = 
+                    {
+                        ["mapSize"] = 1,
+                        ["filters"] = 
+                        {
+                            [1] = 
+                            {
+                            },
+                            [2] = 
+                            {
+                                [1] = false,
+                                [6] = false,
+                                [11] = 1,
+                            },
+                            [3] = 
+                            {
+                            },
+                        },
+                    },
+                    [3] = 
+                    {
+                        ["mapSize"] = 1,
+                        ["filters"] = 
+                        {
+                            [2] = 
+                            {
+                                [8] = false,
+                                [1] = false,
+                                [3] = false,
+                                [4] = false,
+                                [5] = false,
+                                [11] = 1,
+                                [7] = false,
+                            },
+                        },
+                        ["allowHistory"] = false,
+                    },
+                    [4] = 
+                    {
+                        ["mapSize"] = 1,
+                        ["filters"] = 
+                        {
+                            [1] = 
+                            {
+                            },
+                            [2] = 
+                            {
+                                [3] = false,
+                                [2] = false,
+                                [11] = 1,
+                            },
+                            [3] = 
+                            {
+                            },
+                        },
+                        ["allowHistory"] = false,
+                    },
+                    [5] = 
+                    {
+                        ["mapSize"] = 1,
+                        ["filters"] = 
+                        {
+                            [2] = 
+                            {
+                                [8] = false,
+                                [1] = false,
+                                [3] = false,
+                                [11] = 1,
+                            },
+                            [3] = 
+                            {
+                            },
+                        },
+                        ["allowHistory"] = false,
+                    },
+                    ["userMode"] = 2,
+                },
+                ["Chat"] = 
+                {
+                    ["containers"] = 
+                    {
+                        [1] = 
+                        {
+                            ["relPoint"] = 6,
+                            ["width"] = 445,
+                            ["height"] = 267,
+                            ["point"] = 6,
+                            ["y"] = -82,
+                            ["x"] = 0,
+                        },
+                    },
+                    ["version"] = 4,
+                },
+            },
+            ["Bandersnatch Cabbagepatch"] = 
+            {
+                ["PerformanceMeters"] = 
+                {
+                    ["version"] = 1,
+                    ["x"] = -20,
+                    ["point"] = 6,
+                    ["y"] = 20,
+                    ["relPoint"] = 6,
+                },
+                ["DeathRecap"] = 
+                {
+                    ["version"] = 1,
+                    ["recapOn"] = true,
+                },
+                ["GamepadSmithingCreation"] = 
+                {
+                    ["version"] = 2,
+                    ["haveMaterialChecked"] = false,
+                    ["haveKnowledgeChecked"] = false,
+                },
+                ["AutoComplete"] = 
+                {
+                    ["version"] = 3,
+                    ["RecentInteractions"] = 
+                    {
+                    },
+                },
+                ["Dyeing"] = 
+                {
+                    ["sortStyle"] = 1,
+                    ["version"] = 1,
+                    ["showLocked"] = true,
+                },
+                ["Provisioner"] = 
+                {
+                    ["version"] = 1,
+                    ["haveSkillsChecked"] = true,
+                    ["haveIngredientsChecked"] = true,
+                },
+                ["SmithingCreation"] = 
+                {
+                    ["version"] = 2,
+                    ["haveMaterialChecked"] = false,
+                    ["haveKnowledgeChecked"] = true,
+                },
+                ["DurabilityWarner"] = 
+                {
+                    ["version"] = 1,
+                },
+                ["GamepadProvisioner"] = 
+                {
+                    ["version"] = 2,
+                    ["haveSkillsChecked"] = false,
+                    ["haveIngredientsChecked"] = false,
+                },
+                ["WorldMap"] = 
+                {
+                    ["version"] = 4,
+                    [1] = 
+                    {
+                        ["width"] = 488,
+                        ["x"] = 0,
+                        ["mapSize"] = 2,
+                        ["point"] = 128,
+                        ["relPoint"] = 128,
+                        ["filters"] = 
+                        {
+                            [1] = 
+                            {
+                            },
+                            [2] = 
+                            {
+                                [1] = false,
+                                [6] = false,
+                                [11] = 1,
+                            },
+                            [3] = 
+                            {
+                            },
+                        },
+                        ["y"] = 0,
+                        ["keepSquare"] = true,
+                        ["height"] = 550,
+                    },
+                    [2] = 
+                    {
+                        ["mapSize"] = 1,
+                        ["filters"] = 
+                        {
+                            [1] = 
+                            {
+                            },
+                            [2] = 
+                            {
+                                [1] = false,
+                                [6] = false,
+                                [11] = 1,
+                            },
+                            [3] = 
+                            {
+                            },
+                        },
+                    },
+                    [3] = 
+                    {
+                        ["mapSize"] = 1,
+                        ["filters"] = 
+                        {
+                            [2] = 
+                            {
+                                [8] = false,
+                                [1] = false,
+                                [3] = false,
+                                [4] = false,
+                                [5] = false,
+                                [11] = 1,
+                                [7] = false,
+                            },
+                        },
+                        ["allowHistory"] = false,
+                    },
+                    [4] = 
+                    {
+                        ["mapSize"] = 1,
+                        ["filters"] = 
+                        {
+                            [1] = 
+                            {
+                            },
+                            [2] = 
+                            {
+                                [11] = 1,
+                                [2] = false,
+                                [3] = false,
+                            },
+                            [3] = 
+                            {
+                            },
+                        },
+                        ["allowHistory"] = false,
+                    },
+                    [5] = 
+                    {
+                        ["mapSize"] = 1,
+                        ["filters"] = 
+                        {
+                            [2] = 
+                            {
+                                [8] = false,
+                                [1] = false,
+                                [11] = 1,
+                                [3] = false,
+                            },
+                            [3] = 
+                            {
+                            },
+                        },
+                        ["allowHistory"] = false,
+                    },
+                    ["userMode"] = 2,
+                },
+                ["Chat"] = 
+                {
+                    ["containers"] = 
+                    {
+                        [1] = 
+                        {
+                            ["relPoint"] = 6,
+                            ["width"] = 445,
+                            ["height"] = 267,
+                            ["point"] = 6,
+                            ["y"] = -82,
+                            ["x"] = 0,
+                        },
+                    },
+                    ["version"] = 4,
+                },
+            },
+        },
+    },
+}
